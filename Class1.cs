8176eac2745c3fb6847c829c690c692fff1982ae
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-04-05 13:02:44 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämninguppgift._2_oop
+{
+    internal class namn
+    {
+        private string helnamn;
+        private string förnamn;
+        private string efternamn;
+
+        public string Förnamn
+        {
+            get { return förnamn; }
+            set { förnamn = value; }
+        }
+
+        public string Efternamn
+        {
+            get { return efternamn; }
+            set { efternamn = value; }
+        }
+
+        public void Namn()
+        {
+            helnamn = förnamn + " " + efternamn;
+            Console.WriteLine("Hej " + helnamn);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Inlämninguppgift._2_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            namn p = new namn();
+            Console.WriteLine("Förnamn?");
+            p.Förnamn = Console.ReadLine();
+            Console.WriteLine("Efternamn");
+            p.Efternamn = Console.ReadLine();
+            Console.WriteLine("");
+            p.Namn(); 
+        }
+    }
+}
